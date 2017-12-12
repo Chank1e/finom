6a6049e5b9b692d54a844c6714a0542d5cea483e
chank1e <ankell.game@gmail.com>
2017-12-12 23:13:29 +0300

    hello world

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var http = require('http');
+/* serves main page */
+app.get("/", function(req, res) {
+   res.sendfile('index.html')
+});
+/* serves all the static files */
+app.get(/^(.+\..+)$/, function(req, res){
+    res.sendfile( __dirname + req.params[0]);
+});
+app.listen(8080, function() {
+  console.log("Listening on 8080");
+});
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter_min = new FileSync('db/min.json')
+const adapter_max = new FileSync('db/max.json')
+const db = {min:low(adapter_min),max:low(adapter_max)};
+
+
+app.get('/api',function(req,res){
+    let $db = (req.query.db==0)?db.min:db.max;
+    let _id = +req.query.id;
+    let result = $db.get('users').find({id:_id}).value();
+    if(result){
+        res.json(result);
+    } else {
+        res.status(204).send('No content!')
+    }
+})
---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <table class="ui celled table">
+      <thead>
+        <tr>
+          <th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td>Cell</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr><th colspan="6">
+          <div class="ui right floated pagination menu">
+            <a class="icon item">
+              <i class="left chevron icon"></i>
+            </a>
+            <a class="item">1</a>
+            <a class="item">2</a>
+            <a class="item">3</a>
+            <a class="item">4</a>
+            <a class="icon item">
+              <i class="right chevron icon"></i>
+            </a>
+          </div>
+        </th>
+      </tr></tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
